e6bb85ae3b53965e570eaedc00eb4580b1adabb4
Arttutu <arthurtt08@gmail.com>
2024-06-27 14:36:06 -0300

    update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function about() {
+export default function About() {
   return (
     <div className="flex flex-col container gap-8">
       <h1 className="text-xl sm:text-3xl text-botao font-font1 font-bold">
-        Um pouco da nossa Cronica
+        Um pouco da nossa Crônica
       </h1>
       <div className="flex flex-col gap-12 sm:flex sm:flex-row sm:gap-12 items-start  mt-12 sm:mt-24">
         <div className="w-full sm:full flex flex-col gap-4">
@@ -18,10 +18,10 @@ export default function about() {
             desde então nunca mais parei de jogar. Os meus gêneros favoritos são
             RPG e estratégia, onde posso me perder em histórias envolventes e
             desafiar minhas habilidades táticas. Entre todos os jogos que já
-            joguei, "The Witcher 3" se destaca como meu favorito absoluto,
-            graças ao seu enredo cativante, personagens memoráveis e um mundo
-            aberto vasto e detalhado. Atualmente, jogo exclusivamente no PC,
-            onde posso aproveitar a melhor qualidade gráfica e uma vasta
+            joguei, &quot;The Witcher 3&quot; se destaca como meu favorito
+            absoluto, graças ao seu enredo cativante, personagens memoráveis e
+            um mundo aberto vasto e detalhado. Atualmente, jogo exclusivamente
+            no PC, onde posso aproveitar a melhor qualidade gráfica e uma vasta
             biblioteca de jogos. Embora nunca tenha participado de torneios,
             minha paixão pelos jogos me levou a criar este blog, onde posso
             compartilhar minhas experiências, reviews e dicas. Meu objetivo com
@@ -37,16 +37,16 @@ export default function about() {
             Daniel
           </h2>
           <p className="text-md sm:text-lg text-paragrafo leading-6  font-font2">
-            Olá, pessoal! Meu nome é Arthur e sou apaixonado por videogames
+            Olá, pessoal! Meu nome é Daniel e sou apaixonado por videogames
             desde que me conheço por gente. Meu primeiro console foi um clássico
             Mega Drive, que abriu as portas para o incrível mundo dos jogos e
             desde então nunca mais parei de jogar. Os meus gêneros favoritos são
             RPG e estratégia, onde posso me perder em histórias envolventes e
             desafiar minhas habilidades táticas. Entre todos os jogos que já
-            joguei, "The Witcher 3" se destaca como meu favorito absoluto,
-            graças ao seu enredo cativante, personagens memoráveis e um mundo
-            aberto vasto e detalhado. Atualmente, jogo exclusivamente no PC,
-            onde posso aproveitar a melhor qualidade gráfica e uma vasta
+            joguei, &quot;The Witcher 3&quot; se destaca como meu favorito
+            absoluto, graças ao seu enredo cativante, personagens memoráveis e
+            um mundo aberto vasto e detalhado. Atualmente, jogo exclusivamente
+            no PC, onde posso aproveitar a melhor qualidade gráfica e uma vasta
             biblioteca de jogos. Embora nunca tenha participado de torneios,
             minha paixão pelos jogos me levou a criar este blog, onde posso
             compartilhar minhas experiências, reviews e dicas. Meu objetivo com
