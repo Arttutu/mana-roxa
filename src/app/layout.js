23292dd70befdf885a4625db6f396f18864b1e4c
Arttutu <arthurtt08@gmail.com>
2024-07-13 18:26:47 -0300

    adicionando linkdo google adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,13 @@ const font = Exo_2({
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className={font.className}>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9774681383780198"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className="w-full flex flex-col min-h-screen bg-background">
         <Menu />
         <div className="flex flex-col flex-grow ">
