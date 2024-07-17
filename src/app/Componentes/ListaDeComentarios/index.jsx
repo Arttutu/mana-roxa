d1098b392f5669e22c512d649114eefbffec6540
Arttutu <arthurtt08@gmail.com>
2024-07-17 11:25:03 -0300

    corrigindo bug

--- src/app/Componentes/ListaDeComentarios/index.jsx
+++ src/app/Componentes/ListaDeComentarios/index.jsx
@@ -7,8 +7,8 @@ export default function ListaDeComentarios({ comentarios }) {
   return (
     <ul className="flex flex-col gap-8">
       {comentarios.map((comentario) => (
-        <li className="flex flex-col items-start gap-4">
-          <Comentario comentario={comentario} key={comentario.id} />
+        <li className="flex flex-col items-start gap-4" key={comentario.id}>
+          <Comentario comentario={comentario} />
           <ModalResposta comentario={comentario} />
           <Resposta comentario={comentario} />
           <div className="w-full border-[1px] text-background2/30"></div>
