3c7ebb52e19232d306abfe18c15f8a72d48a21c6
Arttutu <arthurtt08@gmail.com>
2024-07-29 11:03:53 -0300

    adicionando relogio quebrado pagina de erro

--- src/app/error.js
+++ src/app/error.js
@@ -1,14 +1,16 @@
 "use client" // Error components must be Client Components
 
 import { useEffect } from "react"
-
+import broken from "../../public/assets/img/error/broken.png"
+import Image from "next/image"
 export default function Error({ error, reset }) {
   useEffect(() => {
     console.error(error)
   }, [error])
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <Image src={broken} width={400} height={500} alt="relógio quebrado" />
       <h2 className="text-textoPrincipal text-4xl font-bold">
         Ops algo deu errado
       </h2>
